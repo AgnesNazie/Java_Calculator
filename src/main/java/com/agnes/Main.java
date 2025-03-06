b759ef06e9957869abbfc6b5355abff20cc5442d
AgnesNazie <cheagnesmbo@gmail.com>
2025-03-06 14:45:10 +0100

    created my calculator app

--- src/main/java/com/agnes/Main.java
+++ src/main/java/com/agnes/Main.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello to My Calculator App!");
+        System.out.println("Welcome to My Calculator App!");
 
     }
 }
